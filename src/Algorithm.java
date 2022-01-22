7051f9ebe3fe6ac870379bd45bbf9fa1dde2949c
Jakub Zdanowicz <jakub.zdanowicz99@gmail.com>
2022-01-22 23:22:27 +0100

    wodz

--- src/Algorithm.java
+++ src/Algorithm.java
@@ -3,9 +3,9 @@ import java.util.List;
 
 public class Algorithm {
     public static void main(String[] args) {
-        double[] packageSizes = {500, 200, 100, 50};
-//        System.out.println(optimizedProductPackagingTwo(650, packageSizes));
-        System.out.println(optimizedPackagingToString(optimizedProductPackagingTwo(820, packageSizes), packageSizes));
+        double[] packageSizes = {500, 200, 100, 40};
+        System.out.println(optimizedProductPackagingTwo(890, packageSizes));
+        //System.out.println(optimizedPackagingToString(optimizedProductPackagingTwo(890, packageSizes), packageSizes));
     }
 
     static int calculateAmount(double weight, double packageSize) {
@@ -19,51 +19,61 @@ public class Algorithm {
     }
 
     public static StringBuilder optimizedProductPackagingTwo(double weight, double[] packageSizes) {
-        boolean firstAppend = true;
 
+        boolean firstAppend = true;
+        List<Double> calculatedReminders = new ArrayList<>();
+        List<Integer> calculatedAmounts = new ArrayList<>();
         int leastAmount;
         double smallestReminder;
         StringBuilder stringBuilder = new StringBuilder();
         int i = 0;
+
+        leastAmount = calculateAmount(weight, packageSizes[0]);
+        calculatedReminders.add(calculateRemainder(weight, packageSizes[0], leastAmount));
+        smallestReminder = calculatedReminders.get(0);
         while (weight > 0) {
 
             if (isLastIndex(packageSizes, i)) {
                 int amount = calculateAmount(weight, packageSizes[i]);
                 append(packageSizes[i], firstAppend, stringBuilder, amount);
-                weight = updateWeight(weight, packageSizes[i] * amount);
+                weight -= updateWeight(weight, packageSizes[i] * amount);
             } else {
-                List<Double> calculatedReminders = new ArrayList<>();
-                List<Integer> calculatedAmounts = new ArrayList<>();
-
-                leastAmount = calculateAmount(weight, packageSizes[0]);
-                calculatedReminders.add(calculateRemainder(weight, packageSizes[0], leastAmount));
-                smallestReminder = calculatedReminders.get(0);
-
                 int smallestReminderIndex = 0;
-                for (int j = 1; j < packageSizes.length; j++) {
+                for (int j = 0; j < packageSizes.length; j++) {
                     int amount = calculateAmount(weight, packageSizes[j]);
                     double reminder = calculateRemainder(weight, packageSizes[j], amount);
                     if (reminder == 0) {
-                        smallestReminder = reminder;
-                        leastAmount = amount;
-                        smallestReminderIndex = j;
+                        weight -= updateWeight(packageSizes[j], amount);
+                        append(packageSizes[j], firstAppend, stringBuilder, amount);
+                        break;
+                    } else if (reminder < 0) {
+                        if (j != packageSizes.length - 1) {
+                            amount--;
+                        }
+                        if (amount == 0) {
+                            continue;
+                        } else {
+                            weight -= updateWeight(packageSizes[j], amount);
+                            append(packageSizes[j], firstAppend, stringBuilder, amount);
+                            stringBuilder.append(" + ");
+                        }
                     } else if (reminder < smallestReminder || (reminder == smallestReminder && leastAmount > amount)) {
-                        smallestReminder = reminder;
-                        leastAmount = amount;
-                        smallestReminderIndex = j;
-                    }
+                      smallestReminder = reminder;
+                      leastAmount = amount;
+                      smallestReminderIndex = j;
+                  }
                     calculatedReminders.add(reminder);
                 }
                 int amount = calculateAmount(weight, packageSizes[smallestReminderIndex]);
-                append(packageSizes[smallestReminderIndex], firstAppend, stringBuilder, amount);
-                weight = updateWeight(weight, amount * packageSizes[smallestReminderIndex]);
+
+                weight -= updateWeight(weight, amount * packageSizes[smallestReminderIndex]);
             }
         }
         return stringBuilder;
     }
 
     private static double updateWeight(double weight, double amount) {
-        return weight - amount;
+        return weight * amount;
     }
 
     private static void append(double packageSize, boolean firstAppend, StringBuilder stringBuilder, int amount) {
@@ -119,4 +129,9 @@ public class Algorithm {
         }
         return sb;
     }
+//    public StringBuilder newOptimization(double weight, double[] packageSizes) {
+//
+//    }
 }
+
+
